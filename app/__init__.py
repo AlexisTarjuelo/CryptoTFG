75f50627ddd4953abfbe79ac530ccd2bd6aada65
DESKTOP-VLJE3V7\alex_ <Alexis.Tarjuelo@alu.uclm.es>
2025-04-07 11:25:55 +0200

    Se añaden formularios de login y registro sin estilosjunto al modelo de usuario, también se añade una clave csrf para proteger mas la aplicación, que luego irá en las variables de entorno

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,23 @@
 # app/__init__.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('config.Config')
 
     db.init_app(app)
+    csrf.init_app(app)
+
+    # ✅ Aquí registras el blueprint
+    from .routes import auth_bp
+    app.register_blueprint(auth_bp)
 
     with app.app_context():
-        from . import models
         db.create_all()
 
     return app
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-secreta-super-segura'
     SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://sa:NuevaContraseñaSegura@localhost\\SQLEXPRESS/CRIPTOTFG?driver=ODBC+Driver+17+for+SQL+Server"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     BSCSCAN_API_KEY = os.getenv('BSCSCAN_API_KEY', 'clave_api_bscscan_temporal')
